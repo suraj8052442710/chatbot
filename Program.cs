43b36afdf1783b56fd9d37b4c38c6f1a15589adc
suraj8052442710 <97836818+suraj8052442710@users.noreply.github.com>
2023-03-31 10:50:23 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            
+            Console.WriteLine("Hello Sir I am Microsoft Company Bot");
+            Console.WriteLine("For Register in our company please write your name and press enter ");
+            Console.Write("");
+            Console.ReadLine();
+
+            Console.WriteLine("Please write your age and press enter");
+            Console.Write("");
+            Console.ReadLine();
+
+            Console.WriteLine("Write your gmail id carefully and press enter");
+            Console.Write("");
+            Console.ReadLine();
+
+            Console.WriteLine("Enter Your mobile number: press Enter ");
+            Console.Write("");
+            Console.ReadLine();
+
+            Console.WriteLine("You are successfully registered in our company");
+            Console.WriteLine("For any enquiry contact our Microsoft customer care number");
+            Console.WriteLine("1800 102 1100 ");
+            Console.WriteLine("THANK YOU");
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
